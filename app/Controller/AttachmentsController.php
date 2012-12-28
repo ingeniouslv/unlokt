5195b6bf4b63caf0de2eb6656b5a6968facc8052
gamerInTheCode <sk8inghalfro@gmail.com>
2012-12-28 12:28:46 -0800

    Added 'add pics' beneath the gallery.  Managers should use this link to post photos if they don't want a feed associated with the photo.

--- app/Controller/AttachmentsController.php
+++ app/Controller/AttachmentsController.php
@@ -17,4 +17,53 @@ class AttachmentsController extends AppController {
 		$attachments = $this->Attachment->getGallery();
 		echo json_encode($attachments);
 	}
+	public function add() {
+		if ($this->request->is('post')) {
+			$this->Attachment->create();
+			// If we don't have a Spot ID then we can assume this user only manages 1 spot.
+			if (!isset($this->request->data['Attachment']['spot_id'])) {
+				$array_keys = array_keys($this->viewVars['spots_i_manage']);
+				$this->request->data['Attachment']['spot_id'] = $array_keys[0];
+			}
+			// Verify that the current User has permission to post to this Spot.
+			$this->Attachment->Spot->id = $this->request->data['Attachment']['spot_id'];
+			if (!$this->Attachment->Spot->Manager->isManager()) {
+				throw new NotFoundException('You do not have permission to do this.');
+			}
+			
+			// Check if there is a file being uploaded.
+			if (isset($this->request->data['Attachment']['file']) && is_array($this->request->data['Attachment']['file']) && count($this->request->data['Attachment']['file'])) {
+				foreach ($this->request->data['Attachment']['file'] as $file) {
+					// If there exists and error and it's not = 4 (no upload - normal)
+					if ($file['error'] && $file['error'] != 4) {
+						$this->Attachment->invalidate('file', 'There was an error with your upload.');
+					}
+				}
+			}
+			// Now that we've saved the Feed, check if we have files to save.
+			if (isset($this->request->data['Attachment']['file']) && is_array($this->request->data['Attachment']['file']) && count($this->request->data['Attachment']['file'])) {
+				foreach ($this->request->data['Attachment']['file'] as $file) {
+					// If there exists and error and it's not = 4 (no upload - normal)
+					if (substr($file['type'], 0, 6) == 'image/') {
+						$this->Attachment->create(false);
+						// Make this attachment belong to something ...  set the feed ID.
+						$this->Attachment->set(array(
+							'spot_id' => $this->request->data['Attachment']['spot_id'],
+							'mime' => $file['type'],
+							'type' => 'image',
+							'name' => $file['name']
+						));
+						if ($this->Attachment->save()) {
+							// Finally, save the image by performing a convert() on it.
+							convert($file['tmp_name'], store_path('attachment', $this->Attachment->id, 'default.jpg'));
+						} else {
+							// Oh well, there's not a lot we can do right now with this un-saved attachment :(
+						}
+					}
+				}
+			} // end of checking for files to save
+			$this->Session->setFlash('The attachment has been saved', 'alert-success');
+			$this->redirect($this->request->referer() ? $this->request->referer() : $this->webroot);
+		}
+	}
 }

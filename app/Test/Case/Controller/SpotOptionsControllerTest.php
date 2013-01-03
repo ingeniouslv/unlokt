a4faaee74a3f6c2ff7e3f0352d0dc0a368dfc1a6
gamerInTheCode <sk8inghalfro@gmail.com>
2013-01-03 11:06:24 -0800

    Added admin management for spot options.

---
+++ app/Test/Case/Controller/SpotOptionsControllerTest.php
@@ -0,0 +1,77 @@
+<?php
+App::uses('SpotOptionsController', 'Controller');
+
+/**
+ * SpotOptionsController Test Case
+ *
+ */
+class SpotOptionsControllerTest extends ControllerTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+		'app.spot_option',
+		'app.spot',
+		'app.location',
+		'app.plan',
+		'app.attachment',
+		'app.feed',
+		'app.user',
+		'app.active_deal',
+		'app.deal',
+		'app.redemption_code',
+		'app.manager',
+		'app.password_reset',
+		'app.review',
+		'app.spot_follower',
+		'app.happy_hour',
+		'app.payment',
+		'app.category',
+		'app.categories_spot',
+		'app.spot_options_spot'
+	);
+
+/**
+ * testAdminIndex method
+ *
+ * @return void
+ */
+	public function testAdminIndex() {
+	}
+
+/**
+ * testAdminView method
+ *
+ * @return void
+ */
+	public function testAdminView() {
+	}
+
+/**
+ * testAdminAdd method
+ *
+ * @return void
+ */
+	public function testAdminAdd() {
+	}
+
+/**
+ * testAdminEdit method
+ *
+ * @return void
+ */
+	public function testAdminEdit() {
+	}
+
+/**
+ * testAdminDelete method
+ *
+ * @return void
+ */
+	public function testAdminDelete() {
+	}
+
+}

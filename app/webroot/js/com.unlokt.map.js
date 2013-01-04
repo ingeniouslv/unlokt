b8e3af73a74ad925f9bcca6d6e5fc128b38b4a6b
Zach Jones <zach@peacefulcomputing.com>
2013-01-04 14:08:29 -0800

    Testing changes for IE javascript.

--- app/webroot/js/com.unlokt.map.js
+++ app/webroot/js/com.unlokt.map.js
@@ -161,20 +161,20 @@ com.unlokt.map.prototype.addMarkers = function(markerData) {
 
 // checkMarkers() will iterate through the markers and delete any which are out of bounds.
 com.unlokt.map.prototype.checkBoundMarkers = function() {
-	var map_bounds = this.map.getBounds();
-	log(this.markers);
-	_.each(this.markers, function(marker, i) {
-		// If the marker does not fit within the map bounds, remove it from our map.
-		console.log(marker);
-		if (!map_bounds.contains(marker.position)) {
-			marker.setMap(null);
-			this.marker_ids.splice(_.indexOf(this.marker_ids, marker.id), 1);
-			this.markers.splice(i, 1);
-			if (marker.id == this.infowindow.id) {
-				this.infowindow.close();
-			}
-		}
-	}, this);
+	// var map_bounds = this.map.getBounds();
+	// log(this.markers);
+	// _.each(this.markers, function(marker, i) {
+	// 	// If the marker does not fit within the map bounds, remove it from our map.
+	// 	console.log(marker);
+	// 	if (!map_bounds.contains(marker.position)) {
+	// 		marker.setMap(null);
+	// 		this.marker_ids.splice(_.indexOf(this.marker_ids, marker.id), 1);
+	// 		this.markers.splice(i, 1);
+	// 		if (marker.id == this.infowindow.id) {
+	// 			this.infowindow.close();
+	// 		}
+	// 	}
+	// }, this);
 };
 
 // com.unlokt.map.prototype.updateFeed = function() {
